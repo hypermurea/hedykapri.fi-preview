851526903a2c1f038155e815aa101e8a490f264b
Jaakko Santala <jaakko@hypermurea.com>
2018-12-11 22:19:52 +0200

    updated

--- run_local.sh
+++ run_local.sh
@@ -1,2 +1,3 @@
-export DOCKER_VERSION=3.5
+#!/bin/bash
+export JEKYLL_VERSION=3.5
 sudo docker run --name hedykaprijekyll --volume="$PWD:/srv/jekyll" -p 3000:4000 -it jekyll/jekyll:$JEKYLL_VERSION jekyll serve --watch --drafts
