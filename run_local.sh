1573f7d97663780b4dccaaefa73eb4d904a10c21
Jaakko Santala <jaakko@hypermurea.com>
2020-03-28 16:32:35 +0200

    probono menu

--- run_local.sh
+++ run_local.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export JEKYLL_VERSION=3.5
-sudo docker run --name hedykaprijekyll --volume="$PWD:/srv/jekyll" -p 3000:4000 -it jekyll/jekyll:$JEKYLL_VERSION jekyll serve --watch --drafts
+sudo docker run --name hedykaprijekyll --rm --volume="$PWD:/srv/jekyll" -p 3000:4000 -it jekyll/jekyll:$JEKYLL_VERSION jekyll serve --watch --drafts
